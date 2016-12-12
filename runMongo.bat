36ff53d3db582631f2d3747e5d3dc7d2cdd241a8
lukasz.ksiezak@interia.pl <lukasz.ksiezak@interia.pl>
2016-12-12 12:58:14 +0100

    Plotting static data attempt. Changes to packages config

--- runMongo.bat
+++ runMongo.bat
@@ -1,2 +1,2 @@
-cd C:\Program Files\MongoDB\Server\3.2\bin\
-mongod.exe --dbpath=C:\Users\lukas\Repos\plottingInJS\data
+cd C:\Program Files\MongoDB\Server\3.4\bin\
+mongod.exe --dbpath=C:\Users\plluksi\Source\Experimental\jsNodePlotter\data
